f18bf5a3c1da9e156c6f04094bac9a21f96ae6a1
ailiartsua <ailiartsual2@gmail.com>
2023-09-06 21:28:44 +0900

    21316 스피카 구현

--- src/codingtest/etc/BackJoon21316.java
+++ src/codingtest/etc/BackJoon21316.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class BackJoon21316 {
     public static void main(String[] args) throws IOException {
@@ -20,6 +21,31 @@ public class BackJoon21316 {
             map.put(key,keyList);
             map.put(value,valueList);
         }
-        System.out.println(map);
+        int answerNumber = 0;
+        for (Integer key : map.keySet()) {
+            List<Integer> valueList = map.get(key);
+            // value에 인접한 별들의 개수가 3인 number만 뽑아서
+            if(valueList.size() == 3){
+                // sort한 value가 1,2,3 인경우
+                List<Integer> answerList = Arrays.asList(1, 2, 3);
+                List<Integer> valueSizeList = new ArrayList<>();
+                for (Integer value : valueList) {
+                    valueSizeList.add(map.get(value).size());
+                }
+                List<Integer> sortedList = valueSizeList.stream().sorted().collect(Collectors.toList());
+                boolean isRight = true;
+                for (int i = 0; i < 3; i++) {
+                    if(answerList.get(i) != sortedList.get(i)){
+                        isRight = false;
+                    }
+                }
+                if(isRight){
+                    answerNumber = key;
+                    break;
+                }
+            }
+        }
+        System.out.println(answerNumber);
+
     }
 }
