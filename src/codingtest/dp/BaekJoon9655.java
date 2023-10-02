ea1d30ea2814662c76c22bb176974e497f2f1ffa
ailiartsua <ailiartsual2@gmail.com>
2023-10-03 01:48:40 +0900

    백준 문제풀이

---
+++ src/codingtest/dp/BaekJoon9655.java
@@ -0,0 +1,17 @@
+package codingtest.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BaekJoon9655 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        if(n%3 == 1)
+            System.out.println("SK");
+        else{
+            System.out.println("CY");
+        }
+    }
+}
